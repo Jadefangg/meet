1d0c9e50e4f333475da38b793cc46e5e27e48397
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-07-23 20:00:16 +0530

    App.test.js comments

--- __test/App.test.js
+++ __test/App.test.js
@@ -4,9 +4,9 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 describe('<App /> component', () => {
-    test('renders list of events', () => {
-        const AppDOM = render(<App />).container.firstChild;
-        expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+    test('renders list of events', () => { //the actual function which will contain the test code.
+        const AppDOM = render(<App />).container.firstChild; // render the App component and get the container element - dom node
+        expect(AppDOM.querySelector('#event-list')).toBeInTheDocument(); //.toBeInTheDocument() is a matcher that checks if the element is present in the DOM
       });
 
 });
