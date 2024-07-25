3ee621f0ab764c80b197f791b776f334653c2f3b
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-07-25 12:45:01 +0530

    eventlist test and js file have been romoved to start from scratch on 4.4

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -7,4 +7,4 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i); // Example of querying an element by its text
   expect(linkElement).toBeInTheDocument();
-});
+});//commit
