3785b804a589e51ff0c13427d1942fefc4bb6d24
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-07-27 13:15:44 +0530

    CitySearch understanding

---
+++ src/__test__/CitySearch.test.js
@@ -0,0 +1,31 @@
+// src/__tests__/CitySearch.test.js
+
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import CitySearch from '../components/CitySearch';
+
+describe('<CitySearch /> component', () => {
+  let CitySearchComponent;
+  beforeEach(() => {
+    CitySearchComponent = render(<CitySearch />);
+  });
+  test('renders text input', () => {
+    const cityTextBox = CitySearchComponent.queryByRole('textbox');
+    expect(cityTextBox).toBeInTheDocument();//the 2 expect statements are conditions that must be met for the test to pass
+    expect(cityTextBox).toHaveClass('city');//else it will return a failed test!
+  });
+
+  test('suggestions list is hidden by default', () => {
+    const suggestionList = CitySearchComponent.queryByRole('list');
+    expect(suggestionList).not.toBeInTheDocument();
+  });
+
+  test('renders a list of suggestions when city textbox gains focus', async () => {
+    const user = userEvent.setup();
+    const cityTextBox = CitySearchComponent.queryByRole('textbox');
+    await user.click(cityTextBox);
+    const suggestionList = CitySearchComponent.queryByRole('list');
+    expect(suggestionList).toBeInTheDocument();
+    expect(suggestionList).toHaveClass('suggestions');
+  });
+});
