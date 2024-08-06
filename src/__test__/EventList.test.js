826d3c8cdabc8620606986aefad204f52083556d
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-08-06 13:57:52 +0530

    New test and imports 4.5

--- src/__test__/EventList.test.js
+++ src/__test__/EventList.test.js
@@ -1,9 +1,10 @@
 // src/__tests__/EventList.test.js
 //INTEGRATION TEST - EventList.test.js BETWEEN APP AND EVENTLIST COMPONENTS.
 
-import { render } from '@testing-library/react';
+import { render, within,waitFor } from '@testing-library/react';
 import EventList from '../components/EventList';
 import { getEvents } from '../api';
+import App from '../App';
 
 describe('<EventList /> component', () => {
   test('has an element with "list" role', () => {
@@ -37,5 +38,14 @@ expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.lengt
 } );//Eventlist component test ends here
 
 describe('<EventList /> integration', () => {
+  test('renders a list of 32 events when the app is mounted and rendered', async () => {
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+      expect(EventListItems.length).toBe(32);
+    });
+  });
 
 });
