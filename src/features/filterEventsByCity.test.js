620778fc878dfea924920cae5084ce0385d521d0
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-08-16 14:42:49 +0530

    updated imports 4.6

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,6 +1,8 @@
 //src/features/ filterEventsByCity.test.js
 
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import { render } from '@testing-library/react';
+import App from '../App';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
@@ -8,14 +10,16 @@ defineFeature(feature, test => {
 
     test('when user hasn’t searched for a city, show upcoming events from all cities.', ({ given, when, then }) => {
       given('user hasn’t searched for any city', () => {
-  
+
       });
-  
+    let AppComponent
       when('the user opens the app', () => {
+          AppComponent = render(<App />);
+        });
   
       });
   
-      then('the user should see the list of upcoming events.', () => {
+      then('the user should see the list of all upcoming events', () => {
   
       });
     });
