052245ff0918f6f508c8c5dd021cfdceb5dcbb8d
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-08-26 13:39:23 +0530

    scenario 2

--- src/__test__/EndToEnd.test.js
+++ src/__test__/EndToEnd.test.js
@@ -1,8 +1,9 @@
 //EndToEnd.test.js - PUPPETEER!
 import puppeteer from 'puppeteer';
 
-//SCENARIO 1
+
 describe('show/hide an event details', () => {
+  //SCENARIO 1
     test('An event element is collapsed by default', async () => {
         const browser = await puppeteer.launch();
     
@@ -15,6 +16,19 @@ describe('show/hide an event details', () => {
         // if your event's details have a different selector, use it instead of .event .details
         const eventDetails = await page.$('.event .details');
         expect(eventDetails).toBeNull();
+        browser.close();
+      });
+      //SCENARIO 2
+      test('User can expand an event to see its details', async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+    
+        await page.waitForSelector('.event');
+        await page.click('.event .details-btn');
+    
+        const eventDetails = await page.$('.event .details');
+        expect(eventDetails).toBeDefined();
         browser.close();
       });
 });
