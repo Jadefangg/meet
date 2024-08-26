63f17de3d0e89202bac32e7e63e3f2ee25b74f61
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-08-26 13:48:11 +0530

    final puppetteer test file

--- src/__test__/EndToEnd.test.js
+++ src/__test__/EndToEnd.test.js
@@ -1,34 +1,27 @@
-//EndToEnd.test.js - PUPPETEER!
 import puppeteer from 'puppeteer';
 
+describe('show/hide event details', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  });
 
-describe('show/hide an event details', () => {
-  //SCENARIO 1
-    test('An event element is collapsed by default', async () => {
-        const browser = await puppeteer.launch();
-    
-        const page = await browser.newPage();
-        await page.goto('http://localhost:3000/'); //will be runnin gon this server locally.
-    
-        // if your event element has a different selector, use it instead of .event
-        await page.waitForSelector('.event');
-    
-        // if your event's details have a different selector, use it instead of .event .details
-        const eventDetails = await page.$('.event .details');
-        expect(eventDetails).toBeNull();
-        browser.close();
-      });
-      //SCENARIO 2
-      test('User can expand an event to see its details', async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('http://localhost:3000/');
-    
-        await page.waitForSelector('.event');
-        await page.click('.event .details-btn');
-    
-        const eventDetails = await page.$('.event .details');
-        expect(eventDetails).toBeDefined();
-        browser.close();
-      });
+  afterAll(() => {
+    browser.close();
+  });
+
+  test('An event element is collapsed by default', async () => {
+    const eventDetails = await page.$('.event .details');
+    expect(eventDetails).toBeNull();
+  });
+
+  test('User can expand an event to see details', async () => {
+    await page.click('.event .details-btn');
+    const eventDetails = await page.$('.event .details');
+    expect(eventDetails).toBeDefined();
+  });
 });
