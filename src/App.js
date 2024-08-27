521c1315f9a42c13e5e7774b71a5ed6aa1e885b7
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-08-27 11:55:53 +0530

    Alert OOP export and its new state in App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
 
   const fetchData = async () => {
     const allEvents = await getEvents();
