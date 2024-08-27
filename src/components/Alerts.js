e5d9cc8f4afe07c16dfe253d6ff90ca3ce852c99
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-08-27 11:53:18 +0530

    Alert component OOP continues

--- src/components/Alerts.js
+++ src/components/Alerts.js
@@ -32,4 +32,12 @@ class Alert extends Component {
       </div>
     );
   }
+  
 }
+class InfoAlert extends Alert {
+    constructor(props) {
+      super(props);
+      this.color = 'rgb(0, 0, 255)'; // blue
+      this.bgColor = 'rgb(220, 220, 255)'; // light blue
+    }
+  }
